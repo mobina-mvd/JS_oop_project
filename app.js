a51271d32e688d25990871d99dc94da7408bb182
Mobina Movahedi <mobina.mvd22@gmail.com>
2025-12-03 21:45:56 +0330

    alert and validation added

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const title = $("#title");
 const author = $("#author");
 const body = $("#body");
 
+const alert = $("#alert");
+
 const postForm = $("#postForm");
 
+const postList = $("#postList");
+
+// Classes
 class Post {
   constructor(title, author, body) {
     this.title = title;
@@ -12,14 +17,59 @@ class Post {
   }
 }
 
-class UI {}
+class UI {
+  addPostToList(post) {
+    const div = $("<div/>").addClass("col-12 col-md-4").html(`<div class='card'>
+        <div class='card-body'>
+        <h5 class='card-title'>${post.title}</h5>
+        <div class='card-subtitle mb-2 badge text-bg-secondary'>${post.author}</div>
+        <p class='card-text'>${post.body}</p>
+        <button class='post-delete btn btn-link text-danger ps-0'>Delete post</button>
+        </div>
+      </div>`);
+    postList.append(div);
+  }
 
+  clearInputs() {
+    title.val("");
+    author.val("");
+    body.val("");
+  }
+
+  showAlert(message, className) {
+    alert.html("");
+
+    const alertDiv = $("<div></div>")
+      .addClass(`col-md-4 alert alert-${className}`)
+      .html(message);
+    alert.html(alertDiv);
+
+    // console.log(alertDiv);
+
+    setTimeout(() => {
+      alert.html("");
+    }, 3000);
+  }
+}
+
+// ready Codes
 $(document).ready(function () {
   postForm.submit(submitForm);
 });
 
+// functions
 function submitForm(e) {
   e.preventDefault();
   const post = new Post(title.val(), author.val(), body.val());
+  const uI = new UI();
+  if (title.val() === "" || author.val() === "" || body.val() === "") {
+    console.log("All Field are Required!!!");
+    uI.showAlert("All Field are Required!!!", "danger");
+    return;
+  }
+  uI.addPostToList(post);
+  uI.clearInputs();
+
+  uI.showAlert("Post added succesfully!!!", "success");
   console.log(post);
 }
